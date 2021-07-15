f1b1132fda44b9dd6ee87e3e0aca099ade8b9112
Jordan <jordanashment@gmail.com>
2021-07-14 22:08:20 -0600

    Just playing with routes

--- routes/animals.js
+++ routes/animals.js
@@ -2,20 +2,41 @@ const express = require('express')
 const router = express.Router()
 const { createClient } = require('@supabase/supabase-js')
 
+require('dotenv').config();
 // Create a single supabase client for interacting with your database 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.ANON_KEY)
 
 
 router.get('/all', async(req, res, next) => {
-    const { data, error } = await supabase
+const { data, error } = await supabase
     .from('Play')
     .select()
+    
     if (error) {
         console.log(error)
     } else {
         res.send(data)
     }
     next()
+})
+
+router.post('/add', async (req, res, next) => {
+const { data, error } = await supabase
+  .from('Play')
+  .insert([
+      {
+          name: 'Lion',
+          description: 'King of the Savanna',
+          url: 'https://images.pexels.com/photos/1106452/pexels-photo-1106452.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+          emotion: 'Angry',
+          genus: 'Panthera',
+      }
+  ])
+    if (error) {
+        console.log(error)
+    } else {
+        console.log(data)
+    }
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,34 +1,11 @@
 const express = require('express')
-
-
-require('dotenv').config();
+const animal = require('./routes/animals')
 
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-const animal = require('./routes/animals')
-
-const { createClient } = require('@supabase/supabase-js')
-
-// Create a single supabase client for interacting with your database 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.ANON_KEY)
-
-
-app.get('/', async (req, res) => {
-    res.send('Hello World!')
-const { data, error } = await supabase
-  .from('Play')
-  .insert([
-    { name: 'Lion', description: 'King of the Savanna' }
-  ])
-    if (error) {
-        console.log(error)
-    } else {
-        console.log(data)
-    }
-})
 
 app.use('/animals', animal)
 
