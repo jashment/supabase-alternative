46ecfcbd5d5dbc089e456ee360ac324aa170c635
Jordan <jordanashment@gmail.com>
2021-08-13 15:49:08 -0600

    Fix node server start

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require('express')
-const animal = require('./routes/monsters')
+const monster = require('./routes/monsters')
 const cors = require('cors')
 
 
 const app = express()
-app.options(cors())
+
+const corsOptions = {
+    'preflightContinue': true,
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Allow-Origin': '*',
+    'Referrer-Policy': 'no-referer',
+    'Accept': '*'
+}
+app.options('*', cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(cors())
+app.use(cors(corsOptions))
 
-app.use('/monsters', animal)
+app.use('/monsters', monster)
 
 app.listen(3000, () => {
     console.log('listening on port 3000')
